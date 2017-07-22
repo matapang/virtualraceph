230d7fa512499291f568e3603422265367b086be
matapang <anthony.wong06@yahoo.com>
2017-07-22 22:46:36 +0800

    refactor form

--- app/components/FormSubmitRun/index.js
+++ app/components/FormSubmitRun/index.js
@@ -4,12 +4,19 @@ import { Upload, Icon, message, Form, Input, InputNumber, Button, Checkbox, Row,
 import FormWrapper from 'components/FormWrapper';
 import FormItem from 'components/FormItem';
 import UploadPhoto from 'components/UploadPhoto';
-import AppLink from 'components/AppLink'
 
 class FormSubmitRun extends Component {
 
-    handleSubmit = () => {
-        console.log("handles")
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form 2: ', values);
+                if (this.props.onSubmit) {
+                    this.props.onSubmit(values);
+                }
+            }
+        });
     }
 
     handleUploadPhoto = () => {
@@ -25,31 +32,38 @@ class FormSubmitRun extends Component {
                     <Row>
                         <Col xs={8}>
                             <FormItem label="Upload Photo">
-                                <UploadPhoto />
+                                {getFieldDecorator('photo')
+                                    (<UploadPhoto />)}
                             </FormItem>
                         </Col>
                     </Row>
                     <Row>
                         <Col xs={8}>
                             <FormItem label="Distance (km)">
-                                <InputNumber min={0} placeholder="Distance" />
+                                {getFieldDecorator('distance', {
+                                    rules: [{ required: true, message: 'Please input distance' }],
+                                })
+                                    (< InputNumber min={0} placeholder="Distance" />)}
                             </FormItem>
                         </Col>
                     </Row>
                     <Row gutter={5}>
-                        <Col xs={8}>
+                        <Col xs={8}>                           
                             <FormItem label="Hour">
-                                <InputNumber min={0} placeholder="Hour" />
+                                {getFieldDecorator('hour')
+                                (<InputNumber min={0} placeholder="Hour" />)}
                             </FormItem>
                         </Col>
-                        <Col xs={8}>
+                        <Col xs={8}>                            
                             <FormItem label="Minutes">
-                                <InputNumber min={0} max={59} placeholder="Minutes" />
+                                {getFieldDecorator('minutes')
+                                (<InputNumber min={0} max={59} placeholder="Minutes" />)}
                             </FormItem>
                         </Col>
-                        <Col xs={8}>
+                        <Col xs={8}>                            
                             <FormItem label="Seconds">
-                                <InputNumber min={0} max={59} placeholder="Seconds" />
+                                {getFieldDecorator('seconds')
+                                (<InputNumber min={0} max={59} placeholder="Minutes" />)}
                             </FormItem>
                         </Col>
                     </Row>
@@ -57,17 +71,17 @@ class FormSubmitRun extends Component {
                     <Row gutter={5}>
                         <Col xs={24}>
                             <FormItem label="Notes">
-                                <Input.TextArea rows={4} placeholder="Notes" />
+                                {getFieldDecorator('notes') 
+                                (<Input.TextArea rows={4} placeholder="Notes" />)}
                             </FormItem>
                         </Col>
                     </Row>
 
-                    <br/>
-                    <AppLink to="/feeds">
-                        <Button type="primary" htmlType="submit">
-                            Submit Journal
-                    </Button>
-                    </AppLink>
+                    <br />
+                    
+                    <Button type="primary" htmlType="submit">
+                        Submit Journal
+                    </Button>                    
                 </FormWrapper>
             </Form>
         )
@@ -75,7 +89,7 @@ class FormSubmitRun extends Component {
 }
 
 FormSubmitRun.propTypes = {
-
+    onSubmit:PropTypes.func
 }
 
 export default Form.create()(FormSubmitRun);
--- app/containers/SubmitRun/index.js
+++ app/containers/SubmitRun/index.js
@@ -1,13 +1,20 @@
 import React, { PropTypes } from 'react';
+import { push } from 'react-router-redux';
 import FormSubmitRun from 'components/FormSubmitRun';
 import AppLayout from 'components/AppLayout';
 import { connect } from 'react-redux';
 
 export class SubmitRun extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+  onSubmit = (model) => {
+    //TODO, call actual API for submit here
+    this.props.dispatch(push("/feeds"));
+  }
+
   render() {
     return (
       <AppLayout>
-        <FormSubmitRun />
+        <FormSubmitRun onSubmit={this.onSubmit} />
       </AppLayout>
     );
   }
